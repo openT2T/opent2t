901480887de8f995aa6d5a6ead15fa944ad496da
Jason Ginchereau <jasongin@microsoft.com>
2016-08-03 16:24:14 -0700

    Fix npm package name and structure

---
+++ node/tspublish.js
@@ -0,0 +1,27 @@
+// During development, TypeScript source code is in a separate /lib directory,
+// while compiled JavaScript output goes to /build/lib. But when creating an npm
+// package, all the JavaScript files (and type definitions) need to be relative
+// to the package root to enable direct module references. This script copies
+// the built files to the root before publishing, then cleans them up afterward.
+
+var fs = require("fs");
+var shell = require("shelljs");
+
+if (process.argv[2] === "prepublish") {
+    prepublish();
+} else if (process.argv[2] === "postpublish") {
+    postpublish();
+}
+
+function prepublish() {
+    if (fs.existsSync("build/lib")) {
+        shell.cp("-R", "build/lib/*", ".");
+    }
+}
+
+function postpublish() {
+    shell.find("-R", "build/lib/*").forEach(function(file) {
+        file = file.replace("build/lib/", "");
+        shell.rm("-rf", file);
+    });
+}
