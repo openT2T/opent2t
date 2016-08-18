b895f85a414067f3fb6dce05182d067424d6e2b1
Jason Ginchereau <jasongin@microsoft.com>
2016-08-18 11:41:04 -0700

    Add test .js file that was mistakenly omitted

---
+++ node/test/org.opent2t.test.schemas.c/org.opent2t.test.schemas.c.js
@@ -0,0 +1,5 @@
+// In a non-test interface module, this path would be: "opent2t/schema/OcfSchemaReader"
+var OcfSchemaReader = require("../../build/lib/schema/OcfSchemaReader");
+var path = require("path");
+module.exports = OcfSchemaReader.readThingSchemaFromFiles(
+    path.join(__dirname, path.basename(__filename, ".js") + ".raml"));
